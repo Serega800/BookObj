4d22c865b7c043e85f48364a3029fee294bdc191
Serega800 <svs3107@gmail.com>
2021-12-28 15:27:36 +0500

    Class Book created

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Text.Json;
 
 namespace Csharp
 {
     class Program
     {
-        //3) C#
-        //Описать объект "Книга" с параметрами(атрибутами) и методами, которые могут быть у книги на ваше усмотрение
-        //Предоставить листинг кода + реализовать вызов из консоли отображение всех параметров этого объекта в формате JSON        
         class LabelAttribute : Attribute
         {
             public string LabelText { get; set; }
@@ -19,116 +14,48 @@ namespace Csharp
                 LabelText = labelText;
             }
         }
-        //class Library 
-        //{
-        //    public string Name;
-        //    public int Count;
-        //    //public List<Book> Catalogue = new List<Book>();
-
-        //    private readonly Book[] books = new[]
-        //    {
-        //        new Book(1, "And quiet flows the Don", @"https://www.litres.ru/mihail-sholohov/tihiy-don/"),
-        //        new Book(2, "The King Must Die", @"https://www.amazon.co.uk/King-Must-Die-Mary-Renault/dp/0099463520"),
-        //        new Book(2, "Kill the Mogingbird", @"https://bookscafe.net/read/harper_lee-to_kill_a_mockingbird-147382.html#p1") 
-        //    };
-        //    public Library()
-        //    {
-        //        Console.WriteLine("Создание новой библиотеки");
-        //        Name = "Noname";
-        //        Count = books.Length;
-        //    }
-        //    public Library(string name) : this()
-        //    {
-        //        Name = name;
-        //        Count = books.Length;
-        //    }
-        //    public Book[] GetAllById(int id)
-        //    {
-        //        return books.Where(book => book.Id.Equals(id)).ToArray();
-        //    }            
-        //    public Book[] GetAllByTitle(string title)
-        //    {
-        //        return books.Where(book => book.Title.Contains(title)).ToArray();
-        //    }
-        //}
-        //public Library(List<Book> catalogue)
-        //    {
-        //        Catalogue = catalogue;
-        //    }
-        //public Catalog() { catalogue = new Catalogue[0]; }
-
-        //public void Add(Tovar t)
-        //{
-        //    Array.Resize(ref tovar, tovar.Length + 1);
-        //    tovar[tovar.Length - 1] = t;
-        //}
-        public static string MakeIsbn(string isbn) // string must have 9 digits
-        {
-            if (isbn == null)
-                throw new ArgumentNullException();
-                        
-            isbn = isbn.Normalize();
-            if (isbn.Length != 9)
-                throw new ArgumentException();
-
-            int result;
-            for (int i = 0; i != 9; i++)
-                if (!int.TryParse(isbn[i].ToString(), out result))
-                    throw new ArgumentException();
-            int sum = 0;
-            for (int i = 0; i != 9; i++)
-                sum += (i + 1) * int.Parse(isbn[i].ToString());
-
-            int remainder = sum % 11;
-            if (remainder == 10)
-                return isbn + 'X';
-            else
-                return isbn + (char)('0' + remainder);
-        }
         class Book 
         {
             [Label("Название книги")]
-            public string Title { get; set; }
+            public string Title { get; private set; }
             [Label("Автор книги")]
-            public string Author { get; set; }
+            public string Author { get; private set; }
             [Label("Идентификатор книги")]
-            public Guid Id { get; set; }
+            public Guid Id { get; private set; }
             [Label("Международный стандартный книжный номер")]
-            static string Isbn { get; set; }
+            public string isbn;
+            public string Isbn 
+            {
+                get { return isbn; }
+                set
+                {
+                    //if (!isValidIsbn(value)) throw new ArgumentException("isbn number must contain 10 digits");
+                    if (!isValidIsbn(value)) throw new ArgumentException("isbn number must contain 10 digits");
+                    isbn = value;
+                } 
+            }
             [Label("ссылка на книгу")]
             public string Reference { get; set; }
-            //int Rating { get; }            
-            
-            //[Label("Дата издания")]
-            //int Released { get; }
-            //int Sequel_id { get; }
-            //private bool IsRead = false;
-            //[Label("Количество страниц")]
-            //private int Pages = 1;
-            //public void assignId()
-            //{
-            //    Id = isbn;
-            //    isbn++;
-            //}
+            [Label("Год издания")]
+            public int Released { get; set; }
 
-            //public Book(Guid id, string title, string reference)
-            //{
-            //    Id = id;
-            //    //Isbn = isbn;
-            //    Title = title;
-            //    Reference = reference;                
-            //}            
-
-            public Book(string title = "Noname", string author = "Unknown", )
+            public Book(string title = "Noname",
+                string author = "Unknown",
+                string isbn = "007462542X",
+                string reference = @"https://books.google.ru/",
+                int released = 0)
             {
                 Title = title;
                 Author = author;
                 Id = Guid.NewGuid();
+                Isbn = isbn;
+                Reference = reference;
+                Released = released;
             }
 
-            public void GetBookById(int id)
+            public void GetIdByTitle(string title)
             {
-                Console.WriteLine(id);
+                Console.WriteLine($"Название книги: {title}  Id: {Id}");
             }
             public void GetBookByTitle(string title)
             {
@@ -138,43 +65,91 @@ namespace Csharp
             {
                 Console.WriteLine($"Id: {Id}  Title: {Title}");
             }
+            public void PrintAsJson()
+            {
+                string jsonString = JsonSerializer.Serialize(this);
+                Console.WriteLine(jsonString);
+            }
         }
         static void PrintAsJson(object obj)
         {
             string jsonString = JsonSerializer.Serialize(obj);            
             Console.WriteLine(jsonString);
+        }        
+        static bool isValidIsbn(string isbn)
+        {
+            // length must be 10
+            int n = isbn.Length;
+            if (n != 10)
+                return false;
+
+            // Computing weighted sum of
+            // first 9 digits
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                int digit = isbn[i] - '0';
+
+                if (0 > digit || 9 < digit)
+                    return false;
+
+                sum += (digit * (10 - i));
+            }
+
+            // Checking last digit.
+            char last = isbn[9];
+            if (last != 'X' && (last < '0'
+                             || last > '9'))
+                return false;
+
+            // If last digit is 'X', add 10
+            // to sum, else add its value.
+            sum += ((last == 'X') ? 10 :
+                              (last - '0'));
+
+            // Return true if weighted sum
+            // of digits is divisible by 11.
+            return (sum % 11 == 0);
+        }
+        public static bool inputIsValid(ConsoleKeyInfo input)
+        {
+            int num;
+            if (char.IsDigit(input.KeyChar))
+            {
+                num = int.Parse(input.KeyChar.ToString()); // use Parse if it's a Digit                
+            }
+            else
+            {                
+                num = -1;  // Else we assign a default value
+            }
+
+            if(num < 1 || num > 3)
+            {
+                Console.WriteLine("\nДолжна быть введена цифра от 1 до 3");
+                return false;
+            }
+            return true;
         }
         static void Main(string[] args)
         {
-            var book = new Book("And quiet flows the Don", "Sholohov");
-            //var book1 = new Book("And quiet flows the Don", "Sholohov", @"https://www.litres.ru/mihail-sholohov/tihiy-don/");
-            //var book2 = new Book(2, "The King Must Die", @"https://www.amazon.co.uk/King-Must-Die-Mary-Renault/dp/0099463520");
-            //var book3 = new Book(2, "Kill the Mogingbird", @"https://bookscafe.net/read/harper_lee-to_kill_a_mockingbird-147382.html#p1");
-            //MethodInfo method = book1.GetType().GetMethod("Print");
-            //method.Invoke(book1, null);
-            //Console.WriteLine(book);
-
-            //var lib = new Library();
-            //Console.WriteLine(lib);
-            //Console.ReadKey();
-            //Type type = typeof(Book);
-            //Console.WriteLine($"Type: {type}");
-
-            //string jsonString = JsonSerializer.Serialize(book);
-            //Console.WriteLine(book.Title);
-            //book.Print();
-            //Console.WriteLine(jsonString);
-
-            //PrintAsJson(book);
-
-            //Console.WriteLine(book);
-            Console.Write("Введите идентификатор книги для отображения параметров в формате JSON: ");
-            var idFmConsole = Console.ReadLine();
-            Console.WriteLine(idFmConsole.ToString());
-            //PrintAsJson(book.Id);
             
-            Console.WriteLine("Нажмите любую клaвишу для выхода из программы");
-            Console.ReadLine();
+            var books = new List<Book>();
+            books.Add(new Book("And quiet flows the Don", "Mikhail Sholokhov"));
+            books.Add(new Book("The King Must Die", "Mary Reanult", "0394751043", released: 1958));
+            books.Add(new Book("To Kill a Mogingbird", "Harper Lee", "0060935464", @"https://bookscafe.net/read/harper_lee-to_kill_a_mockingbird-147382.html#p1", 1960));
+
+            foreach (var book in books)            
+                Console.WriteLine(book.Title);
+            
+            Console.WriteLine("\nВведите номер книги от 1 до 3, для отображения всех параметров книги в формате JSON");            
+            ConsoleKeyInfo userInput = Console.ReadKey(); // Get user input                                   
+            
+            if (inputIsValid(userInput))
+            {
+                int index = int.Parse(userInput.KeyChar.ToString()) - 1;
+                Console.WriteLine("\n");
+                books[index].PrintAsJson();
+            }            
         }
     }    
 }
